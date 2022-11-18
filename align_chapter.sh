9fd41d23d01e6d4c6a7a8d430ad904fc548020ae
bavagliladri <lucabrigadavilla@gmail.com>
2022-11-18 15:01:24 +0100

    pipeline

--- align_chapter.sh
+++ align_chapter.sh
@@ -15,6 +15,6 @@ src_bin="/home/luca/word-vectors/cc."$LANG_MODEL".300.bin"
 tgt_vec="/home/luca/word-vectors/cc.en.300.vec"
 tgt_bin="/home/luca/word-vectors/cc.en.300.bin"
 
-/home/luca/vecalign/vecalign.py --alignment_max_size 8 --src "$src_text" --tgt "$tgt_text" \
+/home/luca/vecalign/vecalign.py --alignment_max_size 20 --src "$src_text" --tgt "$tgt_text" \
    --src_embed "$src_vec" "$src_bin" \
    --tgt_embed "$tgt_vec" "$tgt_bin" > /home/luca/align-gosact/aligned/"$SRC_LANG"/"$CHAPTER"
